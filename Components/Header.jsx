c2ec770afe2f57227d27291af2eface5a9e3c706
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-19 19:51:30 -0500

    Create Email subscription Feature

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,27 @@
 import { assets } from '@/Assets/assets'
+import axios from 'axios';
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const Header = () => {
+
+  const [email, setEmail] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    const response = await axios.post('/api/email', formData);
+    if (response.data.success) {
+      toast.success(response.data.msg);
+      setEmail("");
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
@@ -12,8 +31,8 @@ const Header = () => {
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Últimos blogs</h1>
         <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae rem veniam ipsa? Suscipit molestias eligendi mollitia, dolor sequi ea doloremque ipsum veniam a ab itaque nemo vitae quas consequatur voluptatem.</p>
-        <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
-          <input type="email" placeholder='Ingresa tu correo' className='pl-4 outline-none' />
+        <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
+          <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder='Ingresa tu correo' className='pl-4 outline-none' />
           <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Suscríbete</button>
         </form>
       </div>
